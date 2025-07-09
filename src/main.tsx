d6d8f6dc2b44b7ef28fb9e4786777b9ad9258f3b
SUBASH <121956112+GITHUB-SUBASHk@users.noreply.github.com>
2025-07-09 23:43:08 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-// File: if/web/src/main.tsx 
 import React from 'react'; 
 import ReactDOM from "react-dom/client"; 
 import App from './App'; 
-import './styles/globals.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")!).
+  render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
